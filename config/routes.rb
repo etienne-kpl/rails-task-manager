01f7936ef5cf3e3b79cc5d8f11ae98f5641f3223
Etienne Klepal <etienne.klepal@gmail.com>
2022-11-15 14:46:23 +0100

    task manager ready to go

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # CREATE
-  ## Affiche une page avec le formulaire
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
-  # READ
-  get 'tasks', to: 'tasks#index'
-  # comme il y a la variable id, on lui donne un alias avec "as:"
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # CREATE
+  # ## Affiche une page avec le formulaire
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+  # # READ
+  # get 'tasks', to: 'tasks#index'
+  # # comme il y a la variable id, on lui donne un alias avec "as:"
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # # UPDATE
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # # DELETE
+  # delete 'tasks/:id', to: 'tasks#destroy'
 
+  ## refacto:
+  resources :tasks
 end
